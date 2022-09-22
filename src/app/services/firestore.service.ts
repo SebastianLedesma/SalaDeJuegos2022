eebff27c4f926687a79c8d9aa0b40c20139e7078
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-09-22 00:00:17 -0300

    Image compressed.

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
+import { Mensaje } from '../componentes/chat/interface/mensaje.interface';
 import { Registro } from '../interfaces/registro';
 
 @Injectable({
@@ -8,10 +10,23 @@ import { Registro } from '../interfaces/registro';
 })
 export class FirestoreService {
 
+
   constructor(private _firestore:Firestore) { }
 
   agregarLog(registro:Registro){
     const registroRef = collection(this._firestore, 'registros');
     return addDoc(registroRef, registro);
   }
+
+  agregarMensaje(mensaje:Mensaje){
+    const mensajeRef = collection(this._firestore,'mensajes');
+    return addDoc(mensajeRef,mensaje);
+  }
+
+
+  obtenerMensajes():Observable<Mensaje[]>{
+    const mensajeRef = collection(this._firestore, 'mensajes');
+    return collectionData(mensajeRef, {idField:'id'}) as Observable<Mensaje[]>;
+  }
+
 }
